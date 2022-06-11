16ea6888b384dd79566a1c35c03a42d88d11cfca
bannyromeo <96330785+bannyromeo@users.noreply.github.com>
2022-06-11 12:22:32 +0530

    import java.util.Scanner; class division {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,add;         a=sc.nextInt();         b=sc.nextInt();         add=a+b;         System.out.println(+add);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class division
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,add;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        add=a+b;
+        System.out.println(+add);
+    }
+}
