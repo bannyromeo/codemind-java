0d190c04cc4a897345fad683911e6ddc88722b1e
bannyromeo <96330785+bannyromeo@users.noreply.github.com>
2022-06-22 17:20:47 +0530

    import java.util.Scanner; class indhu {     public static void main(String ram[])     {         int n,k,i,j,primes_grater_k=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {int count=0;             for(j=2;j<arr[i];j++)             {                 if(arr[i]%j==0)                 {                     count++;                 }             }             if(count==0&&arr[i]>=k)             {                 primes_grater_k++;             }         }         System.out.print(primes_grater_k);     } }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class indhu
+{
+    public static void main(String ram[])
+    {
+        int n,k,i,j,primes_grater_k=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {int count=0;
+            for(j=2;j<arr[i];j++)
+            {
+                if(arr[i]%j==0)
+                {
+                    count++;
+                }
+            }
+            if(count==0&&arr[i]>=k)
+            {
+                primes_grater_k++;
+            }
+        }
+        System.out.print(primes_grater_k);
+    }
+}
