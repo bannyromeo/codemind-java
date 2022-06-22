73d2eb8fbb256740852fe463501ab36066151c90
bannyromeo <96330785+bannyromeo@users.noreply.github.com>
2022-06-22 21:30:42 +0530

    import java.util.Scanner; class indhu {     public static void main(String ram[])     {         int n,d;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         d=(n*(n-3))/2;         System.out.print(d);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class indhu
+{
+    public static void main(String ram[])
+    {
+        int n,d;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        d=(n*(n-3))/2;
+        System.out.print(d);
+    }
+}
